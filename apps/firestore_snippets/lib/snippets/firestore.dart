c2b19f6b883a9d10797377244200a0dfa0b7dcab
Eric Windmill <ewindmill@google.com>
2022-02-26 14:08:55 -0500

    adds Firestore snippets through transactions page

--- apps/firestore_snippets/lib/snippets/firestore.dart
+++ apps/firestore_snippets/lib/snippets/firestore.dart
@@ -195,16 +195,136 @@ class FirestoreSnippets extends DocSnippet implements Firestore {
 
   void addData_updateADocument() {
     // [START add_data_update_a_document]
-    /*
-    val washingtonRef = db.collection("cities").document("DC")
-
-// Set the "isCapital" field of the city 'DC'
-washingtonRef
-        .update("capital", true)
-        .addOnSuccessListener { Log.d(TAG, "DocumentSnapshot successfully updated!") }
-        .addOnFailureListener { e -> Log.w(TAG, "Error updating document", e) }
-   */
-
+    final washingtonRef = db.collection("cites").doc("DC");
+    washingtonRef.update({"capital": true}).then(
+        (value) => print("DocumentSnapshot successfully updated!"),
+        onError: (e) => print("Error updating document $e"));
     // [END add_data_update_a_document]
+  }
+
+  void addData_serverTimestamp() {
+    // [START add_data_server_timestamp]
+    final docRef = db.collection("objects").doc("some-id");
+    final updates = <String, dynamic>{
+      "timestamp": FieldValue.serverTimestamp(),
+    };
+
+    docRef.update(updates).then(
+        (value) => print("DocumentSnapshot successfully updated!"),
+        onError: (e) => print("Error updating document $e"));
+    // [END add_data_server_timestamp]
+  }
+
+  void addData_updateFieldsInNestedObjects() {
+    // [START add_data_update_fields_in_nested_objects]
+    // Assume the document contains:
+    // {
+    //   name: "Frank",
+    //   favorites: { food: "Pizza", color: "Blue", subject: "recess" }
+    //   age: 12
+    // }
+    db
+        .collection("users")
+        .doc("frank")
+        .update({"age": 13, "favorites.color": "Red"});
+    // [END add_data_update_fields_in_nested_objects]
+  }
+
+  void addData_updateElementsInArray() {
+    // [START add_data_update_elements_in_array]
+    final washingtonRef = db.collection("cities").doc("DC");
+
+    // Atomically add a new region to the "regions" array field.
+    washingtonRef.update({
+      "regions": FieldValue.arrayUnion(["greater_virginia"]),
+    });
+
+    // Atomically remove a region from the "regions" array field.
+    washingtonRef.update({
+      "regions": FieldValue.arrayRemove(["east_coast"]),
+    });
+    // [END add_data_update_elements_in_array]
+  }
+
+  void addData_incrementANumericValue() {
+    // [START add_data_increment_a_numeric_value]
+    var washingtonRef = db.collection('cities').doc('DC');
+
+    // Atomically increment the population of the city by 50.
+    washingtonRef.update(
+      {"population": FieldValue.increment(50)},
+    );
+    // [END add_data_increment_a_numeric_value]
+  }
+
+  void transactions_updatingDataWithTransactions() {
+    // [START transactions_updating_data_with_transactions]
+    final sfDocRef = db.collection("cities").doc("SF");
+    db.runTransaction((transaction) async {
+      final snapshot = await transaction.get(sfDocRef);
+      // Note: this could be done without a transaction
+      //       by updating the population using FieldValue.increment()
+      final newPopulation = snapshot.get("population") + 1;
+      transaction.update(sfDocRef, {"population": newPopulation});
+    }).then(
+      (value) => print("DocumentSnapshot successfully updated!"),
+      onError: (e) => print("Error updating document $e"),
+    );
+    // [END transactions_updating_data_with_transactions]
+  }
+
+  void transactions_passingInformationOutOfTransactions() {
+    // [START transactions_passing_information_out_of_transactions]
+
+    // TODO: ewindmill@ - either the above example (using asnyc) or this example
+    // using (then) is "more correct". Figure out which one.
+    final sfDocRef = db.collection("cities").doc("SF");
+    db.runTransaction((transaction) {
+      return transaction.get(sfDocRef).then((sfDoc) {
+        final newPopulation = sfDoc.get("population") + 1;
+        transaction.update(sfDocRef, {"population": newPopulation});
+        return newPopulation;
+      });
+    }).then(
+      (newPopulation) => print("Population increased to $newPopulation"),
+      onError: (e) => print("Error updating document $e"),
+    );
+    // [END transactions_passing_information_out_of_transactions]
+  }
+
+  void transactions_batchedWrites() {
+    // [START transactions_batched_writes]
+    /*
+
+
+
+
+
+
+// Commit the batch
+batch.commit().then(() => {
+    // ...
+});
+   */
+    // Get a new write batch
+    final batch = db.batch();
+
+    // Set the value of 'NYC'
+    var nycRef = db.collection("cities").doc("NYC");
+    batch.set(nycRef, {"name": "New York City"});
+
+    // Update the population of 'SF'
+    var sfRef = db.collection("cities").doc("SF");
+    batch.update(sfRef, {"population": 1000000});
+
+    // Delete the city 'LA'
+    var laRef = db.collection("cities").doc("LA");
+    batch.delete(laRef);
+
+    // Commit the batch
+    batch.commit().then((_) {
+      // ...
+    });
+    // [END transactions_batched_writes]
   }
 }
