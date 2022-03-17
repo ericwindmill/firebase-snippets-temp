8931b3339757eca0b17617e1480467e2e2cb05e8
Eric Windmill <ewindmill@google.com>
2022-03-17 16:09:01 -0700

    actions test

---
+++ .github/workflows/scripts/run-ci.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if ! [ -x "$(command -v firebase)" ]; then
+  echo "❌ Firebase tools CLI is missing."
+  exit 1
+fi
+
+if ! [ -x "$(command -v flutter)" ]; then
+  echo "❌ Flutter tools CLI is missing."
+  exit 1
+fi
