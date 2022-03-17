76c8b0d51f1db6bc1e9aa068c2ae184f5757c5ab
Eric Windmill <ewindmill@google.com>
2022-03-17 15:18:13 -0700

    actions test

--- .github/workflows/scripts/install-flutter.sh
+++ .github/workflows/scripts/install-flutter.sh
@@ -4,5 +4,4 @@
 #git clone https://github.com/flutter/flutter.git --depth 1 -b $BRANCH "$GITHUB_WORKSPACE/_flutter"
 #echo "$GITHUB_WORKSPACE/_flutter/bin" >> $GITHUB_PATH
 
-echo "yoooooooooooooooooooooo"
 printf "oyyyyyyyyyyyyyyyyyyyyyyy"
