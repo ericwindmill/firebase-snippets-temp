38ce0b168cfa76415ead6086903b3befd85122ac
Eric Windmill <ewindmill@google.com>
2022-03-17 15:04:24 -0700

    actions test

--- .github/workflows/scripts/install-tools.sh
+++ .github/workflows/scripts/install-tools.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+
+
 echo "Starting install tools...."
 
 flutter config --no-analytics
+flutter pub global activate melos 1.1.0
+echo "$HOME/.pub-cache/bin" >> $GITHUB_PATH
+echo "$GITHUB_WORKSPACE/_flutter/.pub-cache/bin" >> $GITHUB_PATH
+echo "$GITHUB_WORKSPACE/_flutter/bin/cache/dart-sdk/bin" >> $GITHUB_PATH
