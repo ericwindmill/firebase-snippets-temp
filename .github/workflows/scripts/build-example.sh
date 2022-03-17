583ca58044b05bbd745078fbcec782b94645a358
Eric Windmill <ewindmill@google.com>
2022-03-17 14:00:44 -0700

    actions test

--- .github/workflows/scripts/build-example.sh
+++ .github/workflows/scripts/build-example.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 DEFAULT_TARGET="./test_driver/MELOS_PARENT_PACKAGE_NAME_e2e.dart"
+ANOTHER_VAR="cool~~~~~~~~~~~~~~~~~~~~~~~~~~~``"
 
 ACTION=$1
 TARGET_FILE=${2:-$DEFAULT_TARGET}
@@ -9,6 +10,7 @@ melos bootstrap --scope="*firebase_core*" --scope="$FLUTTERFIRE_PLUGIN_SCOPE"
 
 if [ "$ACTION" == "android" ]
 then
+  echo $ANOTHER_VAR
   melos exec -c 1 --scope="$FLUTTERFIRE_PLUGIN_SCOPE_EXAMPLE" -- \
     flutter build apk $FLUTTER_COMMAND_FLAGS --debug --target="$TARGET_FILE" --dart-define=CI=true --no-android-gradle-daemon
   MELOS_EXIT_CODE=$?
